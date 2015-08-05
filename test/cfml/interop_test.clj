722f24be188c44fb4cde44845e1c83eba8fe6c86
Sean Corfield <sean@corfield.org>
2015-08-04 17:11:59 -0700

    Expand test cases

--- test/cfml/interop_test.clj
+++ test/cfml/interop_test.clj
@@ -8,10 +8,42 @@
   (:require [cfml.interop :refer :all]
             [expectations :refer :all]))
 
-;; simple struct tests
+;; simple struct tests:
+;; ensure conversion from Clojure (keywords), Clojure (strings), Java
 
 (expect {"A" 1 "BC" 2 "DEF" 3}
         (to-clj-struct {:a 1 :bC 2 :DeF 3}))
+
+(expect {"A" 1 "BC" 2 "DEF" 3}
+        (to-clj-struct {"a" 1 "bC" 2 "DeF" 3}))
+
+(expect {"A" 1 "BC" 2 "DEF" 3}
+        (to-clj-struct (doto (java.util.HashMap.)
+                         (.put "a" 1)
+                         (.put "bC" 2)
+                         (.put "DeF" 3))))
+
+;; simple struct access tests
+
+(expect 1 (get (to-clj-struct {:a 1 :bC 2 :DeF 3}) :a))
+
+(expect 2 (get (to-clj-struct {:a 1 :bC 2 :DeF 3}) "bc"))
+
+(expect 3 (get (to-clj-struct {:a 1 :bC 2 :DeF 3}) "DEF"))
+
+;; invocation tests:
+
+(expect 1 ((to-clj-struct {:a 1 :bC 2 :DeF 3}) :a))
+
+(expect 2 ((to-clj-struct {:a 1 :bC 2 :DeF 3}) "Bc"))
+
+(expect 3 ((to-clj-struct {:a 1 :bC 2 :DeF 3}) "dEf"))
+
+;; keys and values tests:
+
+(expect #{1 2 3} (set (vals (to-clj-struct {:a 1 :bC 2 :DeF 3}))))
+
+(expect #{"A" "BC" "DEF"} (set (keys (to-clj-struct {:a 1 :bC 2 :DeF 3}))))
 
 ;; empty value tests
 
