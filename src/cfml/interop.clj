8d606896c2c535926293b5c53db5657bfe31efdd
Sean Corfield <sean@corfield.org>
2015-08-04 18:55:36 -0700

    Streamline nested struct handling
    We no longer need/use a helper function since we can iterate over the
    map directly.

--- src/cfml/interop.clj
+++ src/cfml/interop.clj
@@ -13,18 +13,14 @@
   ([] (CaseInsensitiveMap.))
   ([m] (CaseInsensitiveMap. (reduce-kv (fn [m k v] (assoc m (str/upper-case (name k)) v)) {} m))))
 
-(declare to-clj-struct)
-
-(defn nested-to-clj-struct
-  [[k v]]
-  [k (if v (to-clj-struct v) v)])
-
 (defn to-clj-struct
   "Converts a CFML or Clojure data structure to a case insensitive Clojure struct/map."
   [m]
   (cond (nil? m) (struct-new)
         (instance? java.util.List m) (mapv to-clj-struct m)
-        (instance? java.util.Map m) (into (struct-new) (apply hash-map (mapcat nested-to-clj-struct m)))
+        (instance? java.util.Map m) (into (struct-new)
+                                          (for [[k v] m]
+                                            [k (and v (to-clj-struct v))]))
         (instance? java.math.BigDecimal m) (.doubleValue ^java.math.BigDecimal m)
         (instance? java.math.BigInteger m) (.longValue ^java.math.BigInteger m)
         (and (set? m)
