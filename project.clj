460ba94cd0507272fd78ce69d6acd182c3be06cc
Sean Corfield <sean@corfield.org>
2015-08-10 16:42:34 -0700

    Use tools.nrepl version from Leiningen 2.5.2
    Remove :user profile and println warnings, force 2.5.2 minimum Leiningen version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cfml-interop "0.1.1"
+(defproject cfml-interop "0.1.2-SNAPSHOT"
   :description "CFML/Clojure interop library extracted from World Singles code"
   :url "https://github.com/seancorfield/cfml-interop"
   :license {:name "Eclipse Public License"
@@ -12,8 +12,7 @@
             [refactor-nrepl "1.1.0"]
             [lein-expectations "0.0.8"]]
 
-  :profiles {:user {:dependencies [[org.clojure/tools.nrepl "0.2.10"]]}
-             :dev {:dependencies [[expectations "2.0.7"]
+  :profiles {:dev {:dependencies [[expectations "2.0.7"]
                                   [org.clojure/test.check "0.6.1"]]
                    :injections [(require 'expectations)
                                 (expectations/disable-run-on-shutdown)]}
@@ -22,8 +21,4 @@
 
   :aliases {"test-all" ["with-profile" "dev,default:dev,master,default" "expectations"]}
 
-  :jvm-opts ~(do (println "Ignore the warning about the user-level profile!")
-                 (println "It's temporary until this Leiningen issue is fixed:")
-                 (println "https://github.com/technomancy/leiningen/issues/1748")
-                 (println "")
-                 []))
+  :min-lein-version "2.5.2")
