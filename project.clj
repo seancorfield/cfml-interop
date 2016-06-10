df9b6847626c54af56e89a8831166f1a6713376b
Sean Corfield <sean@corfield.org>
2016-06-09 18:03:59 -0700

    Update project.clj
    Update dependencies, make Clojure "provided".

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject cfml-interop "0.2.0"
+(defproject cfml-interop "0.2.1"
   :description "CFML/Clojure interop library extracted from World Singles code"
   :url "https://github.com/seancorfield/cfml-interop"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]]
 
   :aot [cfml.struct]
 
@@ -12,12 +12,12 @@
             [refactor-nrepl "1.1.0"]
             [lein-expectations "0.0.8"]]
 
-  :profiles {:dev {:dependencies [[expectations "2.0.7"]
-                                  [org.clojure/test.check "0.6.1"]]
+  :profiles {:dev {:dependencies [[expectations "2.1.8"]
+                                  [org.clojure/test.check "0.9.0"]]
                    :injections [(require 'expectations)
                                 (expectations/disable-run-on-shutdown)]}
              :master {:repositories [["snapshots" "https://oss.sonatype.org/content/repositories/snapshots/"]]
-                      :dependencies [[org.clojure/clojure "1.8.0-master-SNAPSHOT"]]}}
+                      :dependencies [[org.clojure/clojure "1.9.0-master-SNAPSHOT"]]}}
 
   :aliases {"test-all" ["with-profile" "dev,default:dev,master,default" "expectations"]}
 
